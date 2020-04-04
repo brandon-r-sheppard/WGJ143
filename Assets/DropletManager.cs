c026e4b9f32dd9ada8818229b9ad40a499ffd6f2
brandon-r-sheppard <brandon_r_sheppard@outlook.com>
2020-04-04 14:03:18 -0400

    Water Physics Complete

---
+++ Assets/DropletManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropletManager : MonoBehaviour
+{
+    public Material _r;
+    public Material _g;
+    public Material _b;
+
+    [Range(1,3)]
+    public int type = 3;
+
+    void Update()
+    {
+        switch (type)
+        {
+            case 1:
+                GetComponent<Renderer>().material = _r;
+                GetComponent<TrailRenderer>().material = _r;
+                break;
+            case 2:
+                GetComponent<Renderer>().material = _g;
+                GetComponent<TrailRenderer>().material = _g;
+                break;
+            case 3:
+                GetComponent<Renderer>().material = _b;
+                GetComponent<TrailRenderer>().material = _b;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/WaterGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterGenerator : MonoBehaviour
+{
+
+    public GameObject waterDroplet;
+    public int numOfWaterDroplets;
+    IEnumerator DropWater()
+    {
+        for(int i = 0; i < numOfWaterDroplets; i++)
+        {
+            GameObject drop = GameObject.Instantiate(waterDroplet);
+            float offset = Random.value / 16;
+            Vector3 pos = new Vector3(Random.value > 0.5 ? transform.position.x - offset : transform.position.x + offset, transform.position.y, 0);
+            drop.transform.position = pos;
+            yield return new WaitForSecondsRealtime(0.05f);
+        }
+    }
+    private void Start()
+    {
+        StartCoroutine(DropWater());
+    }
+}
